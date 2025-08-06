10cea53fa4d21fa95e1a2064526e9a9fc3ddc365
Kim-dr <kagasikimberly@gmail.com>
2025-08-06 12:12:51 +0300

    simple sales.sql database

---
+++ forms/sales.sql
@@ -0,0 +1,8 @@
+--Create a database called salesDB
+CREATE DATABASE salesDB;
+
+--Create a database called schoolsDB
+CREATE DATABASE schoolsDB;
+
+--Drop the database called schoolsDB
+DROP DATABASE schoolsDB; 
